7fb1e8567bef12169ef8f907ff1dfa601e38ff09
Jo Zhouzheng <jo@Jos-MacBook.local>
2018-10-14 13:46:16 -0400

    signup

--- app.rb
+++ app.rb
@@ -29,6 +29,23 @@ require 'twilio-ruby'
 
 enable :sessions
 
+post "/signup" do
+  # code to check parameters
+	#...
+  client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+
+  # Include a message here
+  message = "Hi" + params[:first_name] + ", welcome to BotName! I can respond to who, what, where, when and why. If you're stuck, type help."
+
+  # this will send a message from any end point
+  client.api.account.messages.create(
+    from: ENV["TWILIO_FROM"],
+    to: params[:number],
+    body: message
+  )
+	# response if eveything is OK
+	"You're signed up. You'll receive a text message in a few minutes from the bot. "
+end
 
 #Facebook
 
