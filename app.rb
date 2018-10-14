ded3acc7a89762b105947083f4afb4fbff5072f9
Jo Zhouzheng <jo@Jos-MacBook.wv.cc.cmu.edu>
2018-10-14 12:15:27 -0400

    twio

--- app.rb
+++ app.rb
@@ -154,19 +154,19 @@ get "/incoming/sms" do
 
   if session["counter"] == 1
 ###############????How do you break the text into two? How do you send text first, followed by GIF?
-    message = "Hello curious soul, my name is Freud. I know you are one of those who seeks to deepen the knowledge about yourself. Dream is the small hidden door in the deepest and most intimate sanctum of our souls. I am here to help you interpret and visualize your dreams.
-    You can ask me:
-    🧐 How do you do that?
-    👀 Tell me more about yourself."
-    media = nil
+  message = "Hello curious soul, my name is Freud. I know you are one of those who seeks to deepen the knowledge about yourself. Dream is the small hidden door in the deepest and most intimate sanctum of our souls. I am here to help you interpret and visualize your dreams.
+  You can ask me:
+  🧐 How do you do that?
+  👀 Tell me more about yourself."
+  media = nil
   else
-    media = nil
-    message = determine_response body
+  media = nil
+  message = determine_response body
   end
 
 #Define body
 
- def determine_response (body)
+ def determine_response body
      body = body.downcase.strip
 
      empty_array = []
@@ -177,22 +177,22 @@ get "/incoming/sms" do
   elsif body.include? "how can you help" or body.include? "how do you do" or body.include? ""
   message = "First, I would like to ask you a few questions to get to know you better.
 
-            After that, you will start receiving vivid images and interpretations on your dreams.
+  After that, you will start receiving vivid images and interpretations on your dreams.
 
-            Your dreams will be kept securely in your personal dream collection. As your dream journal grows you can look back not just at your thoughts and feelings but spot patterns that will help you on your journey of self-discovery.
+  Your dreams will be kept securely in your personal dream collection. As your dream journal grows you can look back not just at your thoughts and feelings but spot patterns that will help you on your journey of self-discovery.
 
-            Sound good?"
-   elsif body.include? "tell me more about yourself"
-   message = "....."
-   elsif body.include? "sounds good" or body.include? "let's get started"
-   message = "How often do you remember your dreams? You can say 'everyday','a few times a week', 'barely', 'sometimes', etc."
-   elsif body.include? "everyday" or body.include? "few times a week" or body.include? "barely" or body.include? "rarely" or body.include? "sometimes"
-   message = "Noted. People typically only remember their dreams right after they wake up. That’s why it’s important to keep a dream journal.
+  Sound good?"
+  elsif body.include? "tell me more about yourself"
+  message = "....."
+  elsif body.include? "sounds good" or body.include? "let's get started" or
+  message = "How often do you remember your dreams? You can say 'everyday','a few times a week', 'barely', 'sometimes', etc."
+  elsif body.include? "everyday" or body.include? "few times a week" or body.include? "barely" or body.include? "rarely" or body.include? "sometimes"
+  message = "Noted. People typically only remember their dreams right after they wake up. That’s why it’s important to keep a dream journal.
              Now you are all set to receive the verbal and visual interpretations of your dream. Are you ready for your first dream decoding session?"
-   elsif body.include? "yes" or body.include? "i'm ready" or body.include? "i am ready"
-   message = "Tell me about your dream last night."
-   elsif body.include? "mother" or body.include? "mom"
-   message = "A mother in your dream may represent several things:
+  elsif body.include? "yes" or body.include? "i'm ready" or body.include? "i am ready"
+  message = "Tell me about your dream last night."
+  elsif body.include? "mother" or body.include? "mom"
+  message = "A mother in your dream may represent several things:
 
             1. Your mother herself.
 
