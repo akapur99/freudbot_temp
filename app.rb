69814c8561c3221a85e0a35eb144bc5af29c4649
Jo Zhouzheng <jo@Jos-MacBook.local>
2018-10-14 14:08:06 -0400

    git

--- app.rb
+++ app.rb
@@ -30,8 +30,6 @@ require 'twilio-ruby'
 enable :sessions
 
 post "/signup" do
-  # code to check parameters
-	#...
   client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
 
   # Include a message here
@@ -54,50 +52,14 @@ end
 #   params['hub.challenge'] if ENV["VERIFY_TOKEN"] == params['hub.verify_token']
 # end
 
-# get "/" do
-#   401
-# end
-#
-#
-# error 401 do
-#   "Not allowed!!!"
-# end
+get "/" do
+  401
+end
 
 
-
-
-
-#IBM NLU API
-
-#   # If using IAM
-#   natural_language_understanding = IBMWatson::NaturalLanguageUnderstandingV1.new(
-#   iam_apikey: "38Xw5RwE28WLF-vYJvQm-EAVkxZxOcpuCOeUFlMKMGhP",
-#   version: "2018-03-19"
-# )
-#
-# response = natural_language_understanding.analyze(
-#   text: "Bruce Banner is the Hulk and Bruce Wayne is BATMAN! " \
-#         "Superman fears not Banner, but Wayne",
-#   features: {
-#     "entities" => {},
-#     "keywords" => {}
-#   }
-# ).result
-# puts JSON.pretty_generate(response)
-#
-# def search_IBM_NLU_for body
-#
-#
-#   IBM.configure do |config|
-#     config.key
-#
-#   end
-#
-#   response = natural_language_understanding.analyze(
-#
-#     puts response
-
-
+error 401 do
+  "Not allowed!!!"
+end
 
 
 #Unsplash
@@ -132,28 +94,8 @@ end
 #     images += "<img src='#{ image_thumb.to_s }' /><br/>"
 #     images += "<hr/>"
 #   end
-#   return images
+#   images
 # end
-
-#IBM
-  # natural_language_understanding = IBMWatson::NaturalLanguageUnderstandingV1.new(
-  #   iam_apikey: "38Xw5RwE28WLF-vYJvQm-EAVkxZxOcpuCOeUFlMKMGhP",
-  #   iam_url: "https://gateway-wdc.watsonplatform.net/natural-language-understanding/api",
-  #   version: "2018-03-16"
-  # )
-  #
-  # response = natural_language_understanding.analyze(
-  #   text: "some sample text",
-  #   features: {
-  #     "entities" => {},
-  #     "keywords" => {}
-  #   }
-  # ).result
-  #
-  # response[:features][:keywords]
-  # puts JSON.pretty_generate(response)
-
-#IBM
 
 
 get "/incoming/sms" do
@@ -165,14 +107,13 @@ get "/incoming/sms" do
   body = params[:Body] || ""
   body = body.downcase.strip
 
- if session["counter"] == 1
+  if session["counter"] == 1
   message = "Hello curious soul, my name is Freud. I know you are one of those who seeks to deepen the knowledge about yourself. Dream is the small hidden door in the deepest and most intimate sanctum of our souls. I am here to help you interpret and visualize your dreams.
 
 You can ask me:
 🧐 How do you do that?
 👀 Tell me more about yourself."
-  media = nil
- elsif body.include? "how can you help" or body.include? "how do you do"
+  elsif body.include? "how can you help" or body.include? "how do you do"
   message = "First, I would like to ask you a few questions to get to know you better.
 
 After that, you will start receiving vivid images and interpretations on your dreams.
@@ -180,21 +121,16 @@ After that, you will start receiving vivid images and interpretations on your dr
 Your dreams will be kept securely in your personal dream collection. As your dream journal grows you can look back not just at your thoughts and feelings but spot patterns that will help you on your journey of self-discovery.
 
 Sound good?"
-   media = nil
-  elsif body.include? "tell me more about yourself"
+   elsif body.include? "tell me more about yourself"
    message = "....."
-   media = nil
-  elsif body.include? "sounds good" or body.include? "let's get started"
+   elsif body.include? "sounds good" or body.include? "let's get started"
    message = "How often do you remember your dreams? You can say 'everyday','a few times a week', 'barely', 'sometimes', etc."
-   media = nil
-  elsif body.include? "everyday" or body.include? "few times a week" or body.include? "barely" or body.include? "rarely" or body.include? "sometimes"
+   elsif body.include? "everyday" or body.include? "few times a week" or body.include? "barely" or body.include? "rarely" or body.include? "sometimes"
    message = "Noted. People typically only remember their dreams right after they wake up. That’s why it’s important to keep a dream journal.
              Now you are all set to receive the verbal and visual interpretations of your dream. Are you ready for your first dream decoding session?"
-   media = nil
-  elsif body.include? "yes" or body.include? "i'm ready" or body.include? "i am ready"
+   elsif body.include? "yes" or body.include? "i'm ready" or body.include? "i am ready"
    message = "Tell me about your dream last night."
-   media = nil
- elsif body.include? "mother" or body.include? "mom"
+   elsif body.include? "mother" or body.include? "mom"
    message = "A mother in your dream may represent several things:
 
    1. Your mother herself.
@@ -210,14 +146,11 @@ Pick a representation that you think may match up with your dream given your cur
 Type in a number to see detailed explainations, or type 'mother' to see the whole list.
 
             "
-   media = nil
    # media = search_unsplash_for ('mom')
- elsif body == "2"
+   elsif body == "2"
    message = "As mothers offer shelter, comfort, life, guidance and protection, to see your mother in your dream also represents the nurturing aspect of your own character."
-   media = nil
- else
+   else
    message = ""
-   media = nil
 end
 
 # Build a twilio response object
