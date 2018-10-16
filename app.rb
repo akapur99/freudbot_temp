d1cf9f1ded318dc4394c9e30fde34dead5e1187b
Jo Zhouzheng <jo@Jos-MacBook.local>
2018-10-16 11:27:55 -0400

    before session

--- app.rb
+++ app.rb
@@ -51,73 +51,70 @@ error 401 do
 end
 
 #IBMWatson
-get "/test-nlp" do
+# get "/test-nlp" do
+#
+#   text = "A hero can be anyone. Even a man doing something as simple and reassuring as
+#   putting a coat around a little boy's shoulder to let him know that the world hadn't ended."
+#
+#   response = get_npl_for(text)
+#
+#   puts response.to_json
 
-  text = "A hero can be anyone. Even a man doing something as simple and reassuring as
-  putting a coat around a little boy's shoulder to let him know that the world hadn't ended."
-
-  response = get_npl_for( text )
-
-  puts response.to_json
-
-  keywords = get_keywords_from_response response
-  concepts =  get_concepts_from_response response
-  entities = get_entities_from_response response
-
-  keywords.to_s + concepts.to_s
-end
-
-def get_keywords_from_response resp
-
-  return [] if resp.nil?
-
-
-  keywords = []
-
-  resp["keywords"].each do |kw|
-    keywords << kw["text"]
-  end
-
-  return keywords
-
-end
-
-
-def get_npl_for text
-
-
-  features = {
-   sentiment: {}, keywords: {}, concepts: {}, emotion: {}, entities: {}
-  }
-
-  data = {
-   "features" => features,
-   "text" => text
-  }
-  params = {  "version" => "2018-03-19"  }
-
-  headers = {
-   "Content-Type"=>"application/json"
-  }
-
-  auth = { username: "apikey", password: ENV['WATSON_API_KEY'] }
-
-  data = data.to_json if data.instance_of?(Hash)
-
-  url = ENV["WATSON_URL"]
-  method = "/v1/analyze"
-
-
- response = HTTParty.post(
-   url + method,
-   basic_auth: auth,
-   headers: headers,
-   query: params,
-   body: data
-   )
-
-
-end
+#   keywords = get_keywords_from_response response
+#
+#   keywords.to_s
+# end
+#
+# def get_keywords_from_response resp
+#
+#   return [] if resp.nil?
+#
+#
+#   keywords = []
+#
+#   resp["keywords"].each do |kw|
+#     keywords << kw["text"]
+#   end
+#
+#   return keywords
+#
+# end
+#
+#
+# def get_npl_for text
+#
+#
+#   features = {
+#    sentiment: {}, keywords: {}, concepts: {}, emotion: {}, entities: {}
+#   }
+#
+#   data = {
+#    "features" => features,
+#    "text" => text
+#   }
+#   params = {  "version" => "2018-03-19"  }
+#
+#   headers = {
+#    "Content-Type"=>"application/json"
+#   }
+#
+#   auth = { username: "apikey", password: ENV['WATSON_API_KEY'] }
+#
+#   data = data.to_json if data.instance_of?(Hash)
+#
+#   url = ENV["WATSON_URL"]
+#   method = "/v1/analyze"
+#
+#
+#  response = HTTParty.post(
+#    url + method,
+#    basic_auth: auth,
+#    headers: headers,
+#    query: params,
+#    body: data
+#    )
+#
+# end
 
 
 
@@ -156,81 +153,98 @@ def search_unsplash_for response
   images
 end
 
+get '/incoming/sms' do
+  "Hello World"
+end
 
-get "/incoming/sms" do
+# get "/incoming/sms" do
+#
+#   session["last_intent"] ||= nil
+#   session["counter"] ||= 1
+#   count = session["counter"]
+#
+#   sender = params[:From] || ""
+#   body = params[:Body] || ""
+#   body = body.downcase.strip
+#   message = " "
+#   media = nil
+#
+# # thank_you = ["ddd"]
+# # message=thank_you.sample.to_s
+#
+#    if session["counter"] == 1
+#    message = "Hello curious soul, my name is Freud. I know you are one of those who seeks to deepen the knowledge about yourself.
+#    <br />
+#    Dream is the small hidden door in the deepest and most intimate sanctum of our souls. I am here to help you interpret and visualize your dreams.
+#    <br />
+#    How do I do that? Enter 🧐 to find out more. "
+#    message.split('<br />')
 
-  session["last_intent"] ||= nil
-  session["counter"] ||= 1
-  count = session["counter"]
 
-  sender = params[:From] || ""
-  body = params[:Body] || ""
-  body = body.downcase.strip
-  message = " "
-  media = nil
-
-# thank_you = ["ddd"]
-# message=thank_you.sample.to_s
-
-   if session["counter"] == 1
-   message = "Hello curious soul, my name is Freud. I know you are one of those who seeks to deepen the knowledge about yourself. Dream is the small hidden door in the deepest and most intimate sanctum of our souls. I am here to help you interpret and visualize your dreams.
-
-You can ask me:
-🧐 How do you do that?
-👀 Tell me more about yourself."
-   elsif body.include? "how can you help" or body.include? "how do you"
-   message = "First, I would like to ask you a few questions to get to know you better.
-
-After that, you will start receiving vivid images and interpretations on your dreams.
-
-Your dreams will be kept securely in your personal dream collection. As your dream journal grows you can look back not just at your thoughts and feelings but spot patterns that will help you on your journey of self-discovery.
-
-Sound good?"
-    elsif body.include? "tell me more about yourself"
-    message = "....."
-    elsif body.include? "sounds good" or body.include? "let's get started"
-    message = "How often do you remember your dreams? You can say 'everyday','a few times a week', 'barely', 'sometimes', etc."
-    elsif body.include? "everyday" or body.include? "few times a week" or body.include? "barely" or body.include? "rarely" or body.include? "sometimes"
-    message = "Noted. People typically only remember their dreams right after they wake up. That’s why it’s important to keep a dream journal.
-             Now you are all set to receive the verbal and visual interpretations of your dream. Are you ready for your first dream decoding session?"
-    elsif body.include? "yes" or body.include? "i'm ready" or body.include? "i am ready"
-    message = "Tell me about your dream last night."
-    elsif body.include? "mother" or body.include? "mom"
-    message = "A mother in your dream may represent several things:
-
-   1. Your mother herself.
-
-   2. The feminine part of yourself, the nurturing aspect of your own character.
-
-   3. Your ideal woman.
-
-   4. Your relationship with an important female figure.
-
-Pick a representation that you think may match up with your dream given your current real life situation.
-
-Type in a number to see detailed explainations, or type 'mother' to see the whole list.
-
-#             "
-
-    media = search_unsplash_for ("mom")
-    elsif body.include? "my dream was" || "I dreamt"
-    media = search_unsplash_for (keywords[2])
-    elsif body == "2"
-    message = "As mothers offer shelter, comfort, life, guidance and protection, to see your mother in your dream also represents the nurturing aspect of your own character."
-    media = "https://unsplash.com/photos/Q1zMXEI9V8g"
-    elsif body == "3"
-    message = "..."
-
-    else
-    message = "???"
-    end
+# #future encouters greeting.
+#    # elsif body.nil?
+#    # message = "👋 Hi welcome back! What did you dream of last night?"
+#
+#
+#    elsif body == "🧐"
+#    message = "Right after you wake up every morning, I will remind you to log your dreams with me. I will analyze your dream and send you visual and verbal representations of major symbols in your dreams.
+#    <br />
+#    Your dreams will be kept securely in your personal dream collection. As your dream journal grows you can look back not just at your thoughts and feelings but spot patterns that will help you on your journey of self-discovery.
+#    <br />
+#    Ready for your first dream interpretation? Type 👍 to begin; or “menu” to get a list of things you can do. "
+#    elsif body == 👍
+#    message = "Tell me about your dream last night. Try to be as specific as possible. Since we cannot act on our unconscious desires in our waking life, we can explore these feelings in dreams. However, we tend to do this in hidden, symbolic forms. So try to share main symbols appeared in your dream. For example: “my mother”, “dark night”, etc. "
+#    elsif body == "menu"
+#    message = "To start logging and analyzing your dream, type “I dreamt” followed by your dreams.
+#    <br />
+#    📖 To search a particular dream from your dream journal. Enter “search: + keywords”. (e.x: search: mother)
+#    <br />
+#    To learn more about about. enter “Freud”
+#    <br />
+#    To learn common dreams and what supposedly mean, enter “common”
+#    <br />
+#    To hear interesting facts about dreams, enter “facts"
+#
+#    elsif body.include? "mother" or body.include? "mom"
+#    message = "A mother in your dream may represent several things:
+#
+#    1. Your mother herself.
+#
+#    2. The feminine part of yourself, the nurturing aspect of your own character.
+#
+#    3. Your ideal woman.
+#
+#    4. Your relationship with an important female figure.
+#    <br />
+#    Pick a representation that you think may match up with your dream given your current real life situation.
+#    <br />
+#    Type in a number to see detailed explainations, or type 'mother' to see the whole list."
+#
+#    media = search_unsplash_for ("mom")
+#    elsif body.include? "my dream was" or body.include? "I dreamt"
+#    media = get_keywords_from_response get_npl_for(text)
+#    elsif body == "2"
+#    message = "As mothers offer shelter, comfort, life, guidance and protection, to see your mother in your dream also represents the nurturing aspect of your own character."
+#    media = "https://unsplash.com/photos/Q1zMXEI9V8g"
+#    elsif body == "3"
+#    message = "..."
+#    elsif body == "facts"
+#    array_of_lines = IO.readlines("facts.txt")
+#    message = array_of_lines.sample.to_s
+#    elsif body == "common"
+#    array_of_lines = IO.readlines("common.txt")
+#    message = array_of_lines.sample.to_s
+#    else
+#    message = "Sorry I didn't recognize that. Type 'menu' to get a list of options."
+#    end
+#  end
 
 # Build a twilio response object
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
     r.message do |m|
 
       # add the text of the response
-      m.body( message )
+      m.body(message)
 
       # add media if it is defined
       unless media.nil?
